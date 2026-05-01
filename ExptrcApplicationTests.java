f511c8eda098513a224d46e43d9e650a5f43fa7d
Priyanka <146874352+Priyankamanikan@users.noreply.github.com>
2026-05-01 15:19:55 +0530

    Add files via upload

---
+++ ExptrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensetracker.exptrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExptrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
