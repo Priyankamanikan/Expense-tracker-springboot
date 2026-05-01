1f27f1839f477af4a0f403ca53b4e3a0ea1e29a5
Priyanka <146874352+Priyankamanikan@users.noreply.github.com>
2026-05-01 15:13:54 +0530

    Add files via upload

---
+++ Expense.java
@@ -0,0 +1,45 @@
+package com.expensetracker.exptrc.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Expense {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private int id;
+	private double amount;
+	private String category;
+	private String date;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+
+}
