808319261365c7189a64f8d740be6f7c91d7b545
Priyanka <146874352+Priyankamanikan@users.noreply.github.com>
2026-05-01 14:51:03 +0530

    Add files via upload

---
+++ ExptrcApplication.java
@@ -0,0 +1,13 @@
+package com.expensetracker.exptrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExptrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExptrcApplication.class, args);
+	}
+
+}
